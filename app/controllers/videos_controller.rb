def57c7d26bbca89dbe3b5c8dc3f725022f333c1
Liz Dahlstrom <liz@fizzmint.com>
2015-04-13 17:07:14 -0700

    Figured how to set the permit options properly to create and edit entries. Tests on videos currently in a passing or pending state.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,7 +5,22 @@ class VideosController < ApplicationController
   end
 
   def create
-    Video.create params[:title]
+    Video.create params[:video].permit(:title)
     redirect_to :back
+  end
+
+  def edit
+    @video = Video.find params[:id]
+  end
+
+  def update
+    video = Video.find params[:id]
+
+    # Try to update the object
+    if video.update_attributes params[:video].permit(:title)
+      redirect_to videos_path, :notice => 'Your video was updated'
+    else
+      redirect_to :back, :notice => 'There was an error saving your video'
+    end
   end
 end
--- spec/features/videos_spec.rb
+++ spec/features/videos_spec.rb
@@ -14,6 +14,23 @@ describe "Video" do
     end
   end
 
+  describe "edit an existing video" do
+    it "edits a video" do
+      visit videos_path
+      click_link "Edit"
+
+      current_path.should == edit_video_path(@video)
+
+      find_field('Title').value.should == 'Episode 1'
+
+      fill_in 'Title', :with => 'Episode 1 updated!'
+      click_button 'Save'
+
+      current_path.should == videos_path
+      page.should have_content 'Episode 1 updated!'
+    end
+  end
+
   describe "Get /videos" do
     it "display some videos" do
       visit videos_path
